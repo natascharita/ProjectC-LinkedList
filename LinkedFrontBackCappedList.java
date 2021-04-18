c20d5580d07bc9a5bdb3010e00f1dd898814c3f5
Nataxa <33231944+natascharita@users.noreply.github.com>
2021-04-18 14:08:56 -0700

    Add files via upload

---
+++ LinkedFrontBackCappedList.java
@@ -0,0 +1,37 @@
+public class LinkedFrontBackCappedList<T> implements FrontBackCappedListInterface<T> {
+
+	private Node head, tail;
+   
+   // YOUR CLASS HERE!
+
+	public class Node {
+		public T data; 
+		public Node next; 
+
+		private Node(T dataPortion) {
+			data = dataPortion;
+			next = null;
+		}
+
+		private Node(T dataPortion, Node nextNode) {
+			data = dataPortion;
+			next = nextNode;
+		}
+
+		private T getData() {
+			return data;
+		}
+
+		private void setData(T newData) {
+			data = newData;
+		}
+
+		private Node getNextNode() {
+			return next;
+		}
+
+		private void setNextNode(Node nextNode) {
+			next = nextNode;
+		} 
+	} 
+}
